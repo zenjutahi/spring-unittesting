931febd900a767fd33aab14d779b7083bfffd61d
zenjutahi <zenjutahi@gmail.com>
2022-03-15 14:07:45 +0300

    refactoring unit tests done with mocking

--- unit-testing/src/test/java/com/unit/testing/unittesting/business/SomeBusinessMockTest.java
+++ unit-testing/src/test/java/com/unit/testing/unittesting/business/SomeBusinessMockTest.java
@@ -3,21 +3,24 @@ package com.unit.testing.unittesting.business;
 import com.unit.testing.unittesting.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
 
-    SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
+    @InjectMocks
+    SomeBusinessImpl business;
 
-    @BeforeEach
-    public void before(){
-        business.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_basic() {
