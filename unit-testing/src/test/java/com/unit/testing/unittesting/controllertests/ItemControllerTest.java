e93a39d1a0e4c1e186122d7a872bfead4c2bf126
zenjutahi <zenjutahi@gmail.com>
2022-03-17 13:52:25 +0300

    create simple unit test and using json for expected responce chehing

---
+++ unit-testing/src/test/java/com/unit/testing/unittesting/controllertests/ItemControllerTest.java
@@ -0,0 +1,36 @@
+package com.unit.testing.unittesting.controllertests;
+
+
+import com.unit.testing.unittesting.controller.ItemController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void returnItemTest() throws Exception {
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Book\",\"quantity\":100,\"price\":75}"))
+                .andReturn();
+    }
+
+}
